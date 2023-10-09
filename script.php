4882c66c5fd4c418275eb9cf42b1fce7ffeea1ce
EsterDahri <esterdahri@gmail.com>
2023-10-09 17:48:25 +0200

    change p in h3

--- script.php
+++ script.php
@@ -15,7 +15,7 @@ $wright_word = str_replace($bad_word, $w1, $w2);
 </head>
 
 <body>
-    <p>Word 1: <?php echo $w1; ?></p>
+    <h3>Word: <?php echo $w1; ?></h3>
     <h4>The inserted word is <?php echo strlen($w1) ?> letters long.</h4>
     <h4>The censored word is: <?php echo $wright_word; ?></h4>
 </body>
