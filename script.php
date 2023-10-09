4108a48754ee25d6762e90160b71c1630815ec52
EsterDahri <esterdahri@gmail.com>
2023-10-09 16:56:21 +0200

    print everything in page as written

--- script.php
+++ script.php
@@ -1,1 +1,10 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    Welcome <?php echo $_POST["user_name"]; ?> <br>
+    Your email adress is: <?php echo $_POST["user_email"]; ?>
+    Your password is: <?php echo $_POST["password"]; ?>
+</body>
+
+</html>
